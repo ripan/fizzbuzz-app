faef4a7681ee064ab5f065dadefc2c8a3540c8b8
Ripan <er.ripankumar@gmail.com>
2016-05-23 00:55:52 +0100

    Add angular pagination

--- app/Scripts/app/modules/fizzBuzz/fizzBuzzController.js
+++ app/Scripts/app/modules/fizzBuzz/fizzBuzzController.js
@@ -6,18 +6,28 @@
         $scope.isLoading = true;
         $scope.totalItems = 64;
         $scope.currentPage = 1;
+        $scope.pageSize = 25;
         
         $scope.setPage = function (pageNo) {
             $scope.currentPage = pageNo;
         };
 
-        $scope.$watch('currentPage', function(newValue, oldValue) {
-            FizzBuzz.query({page:$scope.currentPage},function (response) {
+        $scope.getData = function () {
+            alert($scope.currentPage +  "-" + $scope.pageSize);
+            FizzBuzz.query({page:$scope.currentPage, page_size:$scope.pageSize},function (response) {
                 $scope.isLoading = false;
                 $scope.data = response;
             }, function (error) {
                 console.log(error)
             });
+        };
+
+        $scope.$watch('currentPage', function(newValue, oldValue) {
+            $scope.getData();
+        });
+
+        $scope.$watch('pageSize', function(newValue, oldValue) {
+            $scope.getData();
         });
 
     }]);
