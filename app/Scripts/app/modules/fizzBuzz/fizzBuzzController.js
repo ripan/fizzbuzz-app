35b475e9ea622de766ea6303c64a0651fd8a9ba2
Ripan <er.ripankumar@gmail.com>
2016-05-25 01:28:07 +0100

    Add fav

--- app/Scripts/app/modules/fizzBuzz/fizzBuzzController.js
+++ app/Scripts/app/modules/fizzBuzz/fizzBuzzController.js
@@ -9,7 +9,7 @@
         $scope.pageSize = 100;
         $scope.pageSizeOptions = [10, 25, 50, 100];
         $scope.maxViewingValue = 100;
-        $scope.viewingValues = [10, 100, 1000, 10000];
+        $scope.viewingValues = [10, 100, 1000, 10000, 100000000000];
 
         $scope.setPage = function(pageNo) {
             $scope.currentPage = pageNo;
@@ -19,7 +19,7 @@
             $scope.isLoading = true;
             FizzBuzz.query({ page: $scope.currentPage, page_size: $scope.pageSize, maxViewingValue: $scope.maxViewingValue }, function(response) {
                 $scope.isLoading = false;
-                $scope.data = response;
+                $scope.data = $.map(response,function(item){return {name:item, isFavourite:false}});
             }, function(error) {
                 console.log(error);
                 $scope.isLoading = false;
@@ -36,6 +36,7 @@
 
         $scope.$watch('maxViewingValue', function(newValue, oldValue) {
             $scope.getData();
+            $scope.totalItems = newValue;
         });
 
     }]);
