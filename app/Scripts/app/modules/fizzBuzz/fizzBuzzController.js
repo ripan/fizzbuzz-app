8039cb99fa8e4d19e60c8300253ebe544037e727
Ripan <er.ripankumar@gmail.com>
2016-05-25 01:07:32 +0100

    Refactor

--- app/Scripts/app/modules/fizzBuzz/fizzBuzzController.js
+++ app/Scripts/app/modules/fizzBuzz/fizzBuzzController.js
@@ -1,24 +1,28 @@
-﻿(function () {
+﻿(function() {
     'use strict';
 
-    angular.module('FizzBuzzModule').controller('FizzBuzzCtrl', ['$scope', 'FizzBuzz', function ($scope, FizzBuzz) {
+    angular.module('FizzBuzzModule').controller('FizzBuzzCtrl', ['$scope', 'FizzBuzz', function($scope, FizzBuzz) {
 
         $scope.isLoading = true;
         $scope.totalItems = 64;
         $scope.currentPage = 1;
-        $scope.pageSize = 25;
-        
-        $scope.setPage = function (pageNo) {
+        $scope.pageSize = 100;
+        $scope.pageSizeOptions = [10, 25, 50, 100];
+        $scope.maxViewingValue = 100;
+        $scope.viewingValues = [10, 100, 1000, 10000];
+
+        $scope.setPage = function(pageNo) {
             $scope.currentPage = pageNo;
         };
 
-        $scope.getData = function () {
-            alert($scope.currentPage +  "-" + $scope.pageSize);
-            FizzBuzz.query({page:$scope.currentPage, page_size:$scope.pageSize},function (response) {
+        $scope.getData = function() {
+            $scope.isLoading = true;
+            FizzBuzz.query({ page: $scope.currentPage, page_size: $scope.pageSize, maxViewingValue: $scope.maxViewingValue }, function(response) {
                 $scope.isLoading = false;
                 $scope.data = response;
-            }, function (error) {
-                console.log(error)
+            }, function(error) {
+                console.log(error);
+                $scope.isLoading = false;
             });
         };
 
@@ -27,6 +31,10 @@
         });
 
         $scope.$watch('pageSize', function(newValue, oldValue) {
+            $scope.getData();
+        });
+
+        $scope.$watch('maxViewingValue', function(newValue, oldValue) {
             $scope.getData();
         });
 
