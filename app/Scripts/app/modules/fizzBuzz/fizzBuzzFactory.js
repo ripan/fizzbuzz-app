e8c4357b45ba8b739eb875b7dfc32ee63edd1d51
Ripan <er.ripankumar@gmail.com>
2016-05-27 18:25:24 +0100

    fix url issue

--- app/Scripts/app/modules/fizzBuzz/fizzBuzzFactory.js
+++ app/Scripts/app/modules/fizzBuzz/fizzBuzzFactory.js
@@ -1,11 +1,15 @@
 ﻿(function () {
 	'use strict';
-
-	var ProdUrl = 'https://fizzbuzz-app.herokuapp.com/api/v1/fizzbuzz/list';
-	var DevUrl = 'http://fizzbuzz.dev/api/v1/fizzbuzz/list';
+	
+    var apiUrl; 
+	if (window.location.hostname == "fizzbuzz-app.herokuapp.com"){
+		apiUrl = 'https://fizzbuzz-app.herokuapp.com/api/v1/fizzbuzz/list';
+	}else{
+		apiUrl = 'http://fizzbuzz.dev/api/v1/fizzbuzz/list'
+	}
 
 	angular.module('FizzBuzzModule').factory("FizzBuzz", ['$resource', function ($resource) {
-		return $resource(DevUrl, { id: '@id' }, {
+		return $resource(apiUrl, { id: '@id' }, {
 			update: { method: 'PATCH' },
 		});
 	}]);
